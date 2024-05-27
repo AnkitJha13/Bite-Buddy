c49dc9d4a27775e547de2c76d88a6d4a544f54fe
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-05-27 17:31:44 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,38 +7,39 @@ function Footer() {
       <div className="footer" id="contact-us">
         <div className="footer-content">
           <div className="footer-content-left">
-            <img src={assets.logo} alt="logo" />
+            <h2>CONNECT</h2>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Perspiciatis earum eum libero iure voluptatem. Placeat omnis
-              necessitatibus amet pariatur numquam?
+              Follow us on social media for the latest updates and exclusive
+              offers. Have questions or feedback? Reach out to our support team
+              anytime. We're here to make your food booking experience seamless
+              and enjoyable!
             </p>
             <div className="footer-social-icons">
               <img src={assets.facebook_icon} alt="fb" />
-              <img src={assets.linkedin_icon} alt="ln" />
-              <img src={assets.twitter_icon} alt="tw" />
+              <a href="https://www.linkedin.com/in/ankit-jha-55353021b/"><img src={assets.linkedin_icon} alt="ln" /></a>
+              <a href="https://x.com/ankitjha1403"><img src={assets.twitter_icon} alt="tw" /></a>
             </div>
           </div>
           <div className="footer-content-center">
             <h2>COMPANY</h2>
             <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Delivery</li>
-                <li>Privacy Policy</li>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Delivery</li>
+              <li>Privacy Policy</li>
             </ul>
           </div>
           <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
+            <h2>CONTACT US</h2>
             <ul>
-                <li>+91 67690 98264</li>
-                <li>tomato.contact@gmail.com</li>
+              <li>+91 67690 98264</li>
+              <li><a href="mailto:foodify.contact@gmail.com">foodify.contact@gmail.com</a></li>
             </ul>
           </div>
         </div>
         <hr />
         <p className="footer-copyright">
-            Copyright 2024 @ Tomato.com - All Right Reserved.
+          Copyright 2024 @ Foodify.com - All Right Reserved.
         </p>
       </div>
     </>
